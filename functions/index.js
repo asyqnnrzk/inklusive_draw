6ce696bf4ad9d04f39a6891e6ed84ef35a7dbe7e
asyqnnrzk <b032110319@student.utem.edu.my>
2024-08-30 00:16:18 +0800

    admin dashboard is finished but theres problem with access admin requests

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // Check if the request is made by an authenticated admin
+  if (context.auth.token.admin !== true) {
+    return {error: "Only admins can grant admin privileges."};
+  }
+
+  // Get the user by email and add the custom claim
+  return admin.auth().getUserByEmail(data.email).then((user) => {
+    return admin.auth().setCustomUserClaims(user.uid, {admin: true});
+  }).then(() => {
+    return {message: `Success! ${data.email} has been made an admin.`};
+  }).catch((error) => {
+    return {error: error.message};
+  });
+});
+
